de6707729a0fdbd0fd30d4de36c9b98222ef7dc4
pudissss <shanu.pudi@gmail.com>
2025-10-25 08:38:18 +0530

    edit333

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoBase = process.env.VITE_BASE || '/cicd_exam/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: repoBase,
   plugins: [react()],
 })
